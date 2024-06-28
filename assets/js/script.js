135705107c191e5230a72730c4a388a7ad9ff70d
Mark <markc9309@yahoo.com>
2024-06-28 23:18:29 +0100

    Refactor: Added doc strings to functions. Removed unwanted codes and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,11 @@ function startGame() {
 
 }
 
+/**
+ * Gathers all the elements and cards in one place. 
+ * Loops through the cards and give value to created elements
+ * appends elements to the DOM
+ */
 function addElementsToGameArea() {
 
     let cards = getPlayingCards();
@@ -38,8 +43,13 @@ function addElementsToGameArea() {
     const append = gameArea[0].appendChild(card.cloneNode());
     append.appendChild(frontCard.cloneNode());
     append.appendChild(backCard.cloneNode());
+    // append.addEventListener('click', flipCard);
 }
 
+/**
+ * Define an array of objects that contains card details
+ * @returns a list of playing cards
+ */
 function getPlayingCards() {
 
     const listOfCards = [{
@@ -55,32 +65,28 @@ function getPlayingCards() {
     return listOfCards;
 }
 
+/**
+ * Define all the elements that are going to be placed on the DOM
+ * @returns elements to be created
+ */
 function createElementsForGameArea() {
-
-    // Define all the elements that are going to be placed on the DOM
 
     // Specify a new card div to be created
     let createCardDiv = document.createElement("div");
     // // Add the card class to the div
     createCardDiv.classList.add("card");
-    // Set the data attribute 
-    // newCardDiv.dataset.card = Object.keys(cardContent)[1];
 
     // Create new img element for front of
     // card content to be placed inside the card div
     let frontOfCardContent = document.createElement("img");
     // Add the front-face class to the img
     frontOfCardContent.classList.add("front-face");
-    // add the source to the img 
-    //newCardContent.src = cardContent.jack;
 
     // Create new img element for back of
     // card content to be placed inside the card div
     let backOfCardContent = document.createElement("img");
     // Add the back-face class to the img
     backOfCardContent.classList.add("back-face");
-    // Add src of img
-    // backOfCardContent.src = "cardFace";
 
     return [createCardDiv, frontOfCardContent, backOfCardContent]
 }
