afda252841d06b53bfe56a584a4df1a27d6de917
Mark <markc9309@yahoo.com>
2024-06-28 22:43:02 +0100

    Feat: Added functionlaity to append cards to game-area

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
  */
 window.addEventListener('load', function () {
     let startGameBtn = document.getElementById('start-game-btn');
-    startGameBtn.addEventListener('click', startModal);
+    startGameBtn.addEventListener('click', startGame);
 });
 
 const card = document.getElementsByClassName('card');
@@ -21,3 +21,58 @@ function flipCard() {
 function startModal() {
     alert("Hello");
 };
+
+function startGame() {
+
+    let cards = getPlayingCards();
+    let elements = createElementsForGameArea();
+    let gameArea = document.getElementsByClassName("game-area");
+    let append = gameArea[0].appendChild(elements[0].cloneNode());
+    console.log(append);
+
+}
+
+function getPlayingCards() {
+
+    const listOfCards = [{
+            src: "assets/images/back_of_card.jpg",
+            alt: "PGA Golfer"
+        },
+        {
+            src: "assets/images/golfers/john_daly.png",
+            data: "daly",
+            alt: "John Daly"
+        }
+    ];
+    return listOfCards;
+}
+
+function createElementsForGameArea() {
+
+    // Define all the elements that are going to be placed on the DOM
+
+    // Specify a new card div to be created
+    let createCardDiv = document.createElement("div");
+    // // Add the card class to the div
+    createCardDiv.classList.add("card");
+    // Set the data attribute 
+    // newCardDiv.dataset.card = Object.keys(cardContent)[1];
+
+    // Create new img element for front of
+    // card content to be placed inside the card div
+    let frontOfCardContent = document.createElement("img");
+    // Add the front-face class to the img
+    frontOfCardContent.classList.add("front-face");
+    // add the source to the img 
+    //newCardContent.src = cardContent.jack;
+
+    // Create new img element for back of
+    // card content to be placed inside the card div
+    let backOfCardContent = document.createElement("img");
+    // Add the back-face class to the img
+    backOfCardContent.classList.add("back-face");
+    // Add src of img
+    backOfCardContent.src = "cardFace";
+
+    return [createCardDiv, frontOfCardContent, backOfCardContent]
+}
