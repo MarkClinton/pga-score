33a4a23ed8885d6238cfb04f08cab5c6ec82f4a1
Mark <markc9309@yahoo.com>
2024-06-28 21:43:45 +0100

    Feat: Added card to game area with listener that toggles class flip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 /**
  * Wait for the DOM to finish loading before running the game.
- * get the button elements and add eventListeners to them.
+ * get the button element and add eventListeners to it.
  */
 window.addEventListener('load', function () {
     let startGameBtn = document.getElementById('start-game-btn');
     startGameBtn.addEventListener('click', startModal);
 });
+
+const card = document.getElementsByClassName('card');
+card[0].addEventListener('click', flipCard);
+
+function flipCard() {
+    this.classList.toggle('flip');
+}
 
 /**
  * The beginning of the game. Function to display a popup modal to capture
